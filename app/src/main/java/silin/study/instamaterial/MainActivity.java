30eb97506e70be6aa67f3404c5850128eed34937
Silin Na <n1207n@gmail.com>
2015-01-12 20:04:47 -0800

    Created button drawables for default and floating action for Pre-21 and 21 API levels. + Created a ToolBar to replace Actionbar and Created an ImageButton.

--- app/src/main/java/silin/study/instamaterial/MainActivity.java
+++ app/src/main/java/silin/study/instamaterial/MainActivity.java
@@ -2,16 +2,27 @@ package silin.study.instamaterial;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.main_toolbar) Toolbar mMainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mMainToolbar);
+        mMainToolbar.setNavigationIcon(R.drawable.ic_menu_white);
     }
 
 
@@ -19,6 +30,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem inboxMenuItem = menu.findItem(R.id.action_inbox);
+        inboxMenuItem.setActionView(R.layout.menu_item_view);
         return true;
     }
 
@@ -30,7 +43,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_inbox) {
             return true;
         }
 
